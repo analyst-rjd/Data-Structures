339f7ef0808be37eccf95a8ca174d42b901f5920
Raj Das Munshi <rajdasmunshi@yahoo.com>
2024-11-29 08:22:50 +0530

    Enabled Sorting Algorithms

--- Sorting Algorithm.cpp
+++ Sorting Algorithm.cpp
@@ -64,3 +64,70 @@ int main(){
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int n){
+    cout<<"The sorted students are:"<<endl;
+
+    for(int i=0;i<n;++i){
+        int mkshift=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[mkshift])
+            mkshift=j; 
+        }
+        swap(arr[i], arr[mkshift]);
+    }
+}
+
+void BubbleSort(int arr[], int n){
+    cout<<"The sorted students with Bubble Sort are:"<<endl;
+
+    for(int i=0;i<n-1;++i){
+        for(int j=0; j<n-i-1;++j){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+void Display(int arr[], int n){
+    for(int i=0; i<n; ++i){
+        cout<<arr[i]<<endl;
+    }
+}
+
+void Top5(int arr[], int n){
+    cout<<"Top 5 students:"<<endl;
+    
+    for(int i=n-1; i>n-6; --i){
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of students:";
+    cin>>n;
+
+    int percentages[n];
+
+    cout<<"Enter the percentages of the students:\n";
+
+    for(int i=0; i<n; ++i){
+        
+        cin>>percentages[i];
+    }
+
+    SelectionSort(percentages, n);
+    Display(percentages, n);
+
+    BubbleSort(percentages, n);
+    Display(percentages, n);
+
+    Top5(percentages, n);
+
+    return 0;
+}
